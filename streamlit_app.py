81ac3b4ef4b4a34883994b9878fe5e8268ab6e14
VondyBland <117369939+VondyBland@users.noreply.github.com>
2024-10-07 11:21:34 +0000

    test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,26 @@ import streamlit as st
 
 st.title("ABC-TESTING")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Тестирование"
 )
 import pandas as pd
 file = st.file_uploader("Drop your CSV here to load to Snowflake", type={"csv"})
 import plotly.express as px
+from streamlit_extras.dataframe_explorer import dataframe_explorer
+
+def example_one():
+    global filtered_df
+    filtered_df = dataframe_explorer(df, case=False)
+    st.dataframe(filtered_df, use_container_width=True)
 
 if file != None: 
     df = pd.read_csv(file)
+    example_one()
+
     df['Соинвест'] = abs(df['Соинвест'])
 
     # Создаем интерактивную визуализацию
-    fig = px.scatter(df,
+    fig = px.scatter(filtered_df,
                     x='Выручка_итог',
                     y='Соинвест',
                     color='full_ABC',
